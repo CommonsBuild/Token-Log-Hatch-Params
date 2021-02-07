5c1d30289b9cd2517b516d0d94d45a17bdf4337c
Shawn Anderson <shawnltf@pm.me>
2021-02-07 11:59:02 -0800

    Refactor and add setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='TEC Hatch',
+      version='0.0.1',
+      description='CAD verification modeling for the TEC Hatch.',
+      author='@tecmns',
+      author_email='',
+      url='https://github.com/TECommons/Token-Log-Hatch-Params',
+      packages=['tech'],
+      long_description=open('README.md').read(),
+     )
