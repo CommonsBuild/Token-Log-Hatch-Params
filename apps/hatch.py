34fddf959a025db00f3e82aa4fd637d7db7b1b2a
Fábio Mendes <ms.fabiomendes@gmail.com>
2021-03-13 17:37:59 -0300

    Update folder structure

--- apps/hatch.py
+++ apps/hatch.py
@@ -1,8 +1,6 @@
-from tech.tech import read_impact_hour_data, read_cstk_data, TECH
-from tech.tech import ImpactHoursData, ImpactHoursFormula, Hatch, DandelionVoting
+from operator import index
 from jinja2 import Environment, FileSystemLoader
 from bokeh.plotting import curdoc
-import tech.config_bounds as config_bounds
 from dotenv import load_dotenv
 import pandas as pd
 import panel as pn
@@ -13,11 +11,19 @@ import urllib.parse as p
 from tabulate import tabulate
 import requests
 import codecs
+import sys
 import os
+
+sys.path.append('..')
+
+from tech.tech import read_impact_hour_data, read_cstk_data, TECH
+from tech.tech import ImpactHoursData, ImpactHoursFormula, Hatch, DandelionVoting
+import tech.config_bounds as config_bounds
+
 
 load_dotenv()
 
-env = Environment(loader=FileSystemLoader('.'))
+env = Environment(loader=FileSystemLoader('..'))
 template = env.get_template('template/index.html')
 
 # API settings
@@ -36,20 +42,30 @@ i = ImpactHoursData()
 # TECH
 t = TECH(total_impact_hours=i.total_impact_hours,
          impact_hour_data=impact_hour_data_1, total_cstk_tokens=8500,
-         config=config_bounds.test_hatch['tech'])
+         config=config_bounds.hatch['tech'])
+
+
+# ImpactHoursFormula
+#impact_hours_rewards = ImpactHoursFormula(i.total_impact_hours, impact_hour_data_1)
+#impact_rewards_view = pn.Column(impact_hours_rewards.impact_hours_rewards,
+# impact_hours_rewards.redeemable,
+# impact_hours_rewards.cultural_build_tribute)
 
 # Hatch
 cstk_data = read_cstk_data()
+#hatch = Hatch(cstk_data, impact_hours_rewards.target_raise,
+# i.total_impact_hours,
+# impact_hours_rewards.target_impact_hour_rate)
 
 # DandelionVoting
-dandelion = DandelionVoting(17e6,config=config_bounds.test_hatch['dandelion_voting'])
+dandelion = DandelionVoting(17e6,config=config_bounds.hatch['dandelion_voting'])
 
 # Import Params Button
 import_params_button = pn.widgets.Button(name='Import params', button_type = 'primary')
 import_description = pn.pane.Markdown('<h4>To import the parameters, click on the button below:</h4>')
 
 # Share Button
-comments = pn.widgets.TextAreaInput(max_length=1024, placeholder='Explain your thoughts on why you choose the params...')
+comments = pn.widgets.TextAreaInput(name='Comments', max_length=1024, placeholder='Explain your thoughts on why you choose the params...')
 share_button = pn.widgets.Button(name='Share your results on GitHub!', button_type = 'primary')
 url = pn.widgets.TextInput(name='URL', value = '')
 share_button.js_on_click(args={'target': url}, code='window.open(target.value)')
@@ -96,8 +112,8 @@ def update_result_score(results_button):
     "Hatch exchange rate (TESTTECH/wxDai)", "Hatch tribute (%)", "Support required (%)",
     "Minimum accepted quorum (%)", "Vote duration (days)", "Vote buffer (hours)",
     "Rage quit (hours)", "Tollgate fee (wxDai)"],
-    'Values': [t.target_raise, t.min_max_raise[1],
-    t.min_max_raise[0], t.impact_hour_slope,
+    'Values': [int(t.target_raise), int(t.min_max_raise[1]),
+    int(t.min_max_raise[0]), t.impact_hour_slope,
     t.maximum_impact_hour_rate, t.hatch_oracle_ratio,
     t.hatch_period_days, t.hatch_exchange_rate, t.hatch_tribute,
     dandelion.support_required_percentage, dandelion.minimum_accepted_quorum_percentage, dandelion.vote_duration_days,
@@ -143,7 +159,7 @@ def update_result_score(results_button):
 <h1>Parameters</h1>
 
 {params_table}
-        """.format(params_table=df.to_markdown(index=False))
+        """.format(params_table=df.to_markdown(index=False, floatfmt=".2f"))
 
         string_data = """
 <h1>Results</h1>
@@ -162,7 +178,7 @@ def update_result_score(results_button):
 
 - A CSTK Token holder that has 2000 CSTK can send a max of {max_wxdai_ratio} wxDai to the Hatch
 
-Play with my parameters [here](http://localhost:5006/test-hatch?ihminr={ihf_minimum_raise}&hs={hour_slope}&maxihr={maximum_impact_hour_rate}&ihtr={ihf_target_raise}&ihmaxr={ifh_maximum_raise}&hor={hatch_oracle_ratio}&hpd={hatch_period_days}&her={hatch_exchange_rate}&ht={hatch_tribute}&sr={support_required}&maq={minimum_accepted_quorum}&vdd={vote_duration_days}&vbh={vote_buffer_hours}&rqh={rage_quit_hours}&tfx={tollgate_fee_xdai}).
+Play with my parameters [here](http://localhost:5006/hatch?ihminr={ihf_minimum_raise}&hs={hour_slope}&maxihr={maximum_impact_hour_rate}&ihtr={ihf_target_raise}&ihmaxr={ifh_maximum_raise}&hor={hatch_oracle_ratio}&hpd={hatch_period_days}&her={hatch_exchange_rate}&ht={hatch_tribute}&sr={support_required}&maq={minimum_accepted_quorum}&vdd={vote_duration_days}&vbh={vote_buffer_hours}&rqh={rage_quit_hours}&tfx={tollgate_fee_xdai}).
 
         """.format(comments=comments.value,
         tollgate_fee_xdai=dandelion.tollgate_fee_xdai,
@@ -197,7 +213,7 @@ pn.state.onload(update_params_by_url_query)
 
 # Front-end
 tmpl = pn.Template(template=template)
-tmpl.add_variable('app_title', 'TEC Test Hatch Dashboard')
+tmpl.add_variable('app_title', 'TEC Hatch Dashboard')
 tmpl.add_panel('A', i.impact_hours_accumulation)
 tmpl.add_panel('B', t)
 tmpl.add_panel('C', t.funding_pool_data_view)
@@ -209,5 +225,5 @@ tmpl.add_panel('W', dandelion.vote_pass_view)
 tmpl.add_panel('R', update_result_score)
 tmpl.add_panel('CO', comments)
 tmpl.add_panel('BU', pn.Column(results_button, share_button, url))
-tmpl.servable(title="TEC Test Hatch Dashboard")
+tmpl.servable(title="TEC Hatch Dashboard")
 
--- apps/test-hatch.py
+++ apps/test-hatch.py
@@ -1,4 +1,3 @@
-from operator import index
 from tech.tech import read_impact_hour_data, read_cstk_data, TECH
 from tech.tech import ImpactHoursData, ImpactHoursFormula, Hatch, DandelionVoting
 from jinja2 import Environment, FileSystemLoader
@@ -18,7 +17,7 @@ import os
 
 load_dotenv()
 
-env = Environment(loader=FileSystemLoader('.'))
+env = Environment(loader=FileSystemLoader('..'))
 template = env.get_template('template/index.html')
 
 # API settings
@@ -37,30 +36,20 @@ i = ImpactHoursData()
 # TECH
 t = TECH(total_impact_hours=i.total_impact_hours,
          impact_hour_data=impact_hour_data_1, total_cstk_tokens=8500,
-         config=config_bounds.hatch['tech'])
-
-
-# ImpactHoursFormula
-#impact_hours_rewards = ImpactHoursFormula(i.total_impact_hours, impact_hour_data_1)
-#impact_rewards_view = pn.Column(impact_hours_rewards.impact_hours_rewards,
-# impact_hours_rewards.redeemable,
-# impact_hours_rewards.cultural_build_tribute)
+         config=config_bounds.test_hatch['tech'])
 
 # Hatch
 cstk_data = read_cstk_data()
-#hatch = Hatch(cstk_data, impact_hours_rewards.target_raise,
-# i.total_impact_hours,
-# impact_hours_rewards.target_impact_hour_rate)
 
 # DandelionVoting
-dandelion = DandelionVoting(17e6,config=config_bounds.hatch['dandelion_voting'])
+dandelion = DandelionVoting(17e6,config=config_bounds.test_hatch['dandelion_voting'])
 
 # Import Params Button
 import_params_button = pn.widgets.Button(name='Import params', button_type = 'primary')
 import_description = pn.pane.Markdown('<h4>To import the parameters, click on the button below:</h4>')
 
 # Share Button
-comments = pn.widgets.TextAreaInput(name='Comments', max_length=1024, placeholder='Explain your thoughts on why you choose the params...')
+comments = pn.widgets.TextAreaInput(max_length=1024, placeholder='Explain your thoughts on why you choose the params...')
 share_button = pn.widgets.Button(name='Share your results on GitHub!', button_type = 'primary')
 url = pn.widgets.TextInput(name='URL', value = '')
 share_button.js_on_click(args={'target': url}, code='window.open(target.value)')
@@ -107,8 +96,8 @@ def update_result_score(results_button):
     "Hatch exchange rate (TESTTECH/wxDai)", "Hatch tribute (%)", "Support required (%)",
     "Minimum accepted quorum (%)", "Vote duration (days)", "Vote buffer (hours)",
     "Rage quit (hours)", "Tollgate fee (wxDai)"],
-    'Values': [int(t.target_raise), int(t.min_max_raise[1]),
-    int(t.min_max_raise[0]), t.impact_hour_slope,
+    'Values': [t.target_raise, t.min_max_raise[1],
+    t.min_max_raise[0], t.impact_hour_slope,
     t.maximum_impact_hour_rate, t.hatch_oracle_ratio,
     t.hatch_period_days, t.hatch_exchange_rate, t.hatch_tribute,
     dandelion.support_required_percentage, dandelion.minimum_accepted_quorum_percentage, dandelion.vote_duration_days,
@@ -154,7 +143,7 @@ def update_result_score(results_button):
 <h1>Parameters</h1>
 
 {params_table}
-        """.format(params_table=df.to_markdown(index=False, floatfmt=".2f"))
+        """.format(params_table=df.to_markdown(index=False))
 
         string_data = """
 <h1>Results</h1>
@@ -173,7 +162,7 @@ def update_result_score(results_button):
 
 - A CSTK Token holder that has 2000 CSTK can send a max of {max_wxdai_ratio} wxDai to the Hatch
 
-Play with my parameters [here](http://localhost:5006/hatch?ihminr={ihf_minimum_raise}&hs={hour_slope}&maxihr={maximum_impact_hour_rate}&ihtr={ihf_target_raise}&ihmaxr={ifh_maximum_raise}&hor={hatch_oracle_ratio}&hpd={hatch_period_days}&her={hatch_exchange_rate}&ht={hatch_tribute}&sr={support_required}&maq={minimum_accepted_quorum}&vdd={vote_duration_days}&vbh={vote_buffer_hours}&rqh={rage_quit_hours}&tfx={tollgate_fee_xdai}).
+Play with my parameters [here](http://localhost:5006/test-hatch?ihminr={ihf_minimum_raise}&hs={hour_slope}&maxihr={maximum_impact_hour_rate}&ihtr={ihf_target_raise}&ihmaxr={ifh_maximum_raise}&hor={hatch_oracle_ratio}&hpd={hatch_period_days}&her={hatch_exchange_rate}&ht={hatch_tribute}&sr={support_required}&maq={minimum_accepted_quorum}&vdd={vote_duration_days}&vbh={vote_buffer_hours}&rqh={rage_quit_hours}&tfx={tollgate_fee_xdai}).
 
         """.format(comments=comments.value,
         tollgate_fee_xdai=dandelion.tollgate_fee_xdai,
@@ -208,7 +197,7 @@ pn.state.onload(update_params_by_url_query)
 
 # Front-end
 tmpl = pn.Template(template=template)
-tmpl.add_variable('app_title', 'TEC Hatch Dashboard')
+tmpl.add_variable('app_title', 'TEC Test Hatch Dashboard')
 tmpl.add_panel('A', i.impact_hours_accumulation)
 tmpl.add_panel('B', t)
 tmpl.add_panel('C', t.funding_pool_data_view)
@@ -220,5 +209,5 @@ tmpl.add_panel('W', dandelion.vote_pass_view)
 tmpl.add_panel('R', update_result_score)
 tmpl.add_panel('CO', comments)
 tmpl.add_panel('BU', pn.Column(results_button, share_button, url))
-tmpl.servable(title="TEC Hatch Dashboard")
+tmpl.servable(title="TEC Test Hatch Dashboard")
 
