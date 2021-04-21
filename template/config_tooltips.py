bcd849ab466b6c7e4ea31fe0da5295a5d0e945e1
Fábio Mendes <ms.fabiomendes@gmail.com>
2021-04-21 11:45:31 -0300

    Add href key

--- template/config_tooltips.py
+++ template/config_tooltips.py
@@ -1,17 +1,62 @@
 tooltips = {
-    "target_raise": "Total amount of funds raised or expected amount of funds raised. Theoretical number for the purpose of running the simulation.",
-    "min_raise": "The minimum amount that needs to be collected during the Hatch to create the TEC HatchDAO, if less than this is collected, the funds will be sent back to the addresses that sent it.",
-    "max_raise": "The maximum amount of wxDai that can be sent to the Hatch.",
-    "hatch_oracle_ratio": "The amount of wxDAI (wrapped xDAI) each CSTK Token holder is able to send to the Hatch per CSTK they have.",
-    "hatch_period_days": "How long the Hatch is open to collect funds.",
-    "hatch_exchange_rate": "The amount of TESTTECH that are obtained for 1 wxDAI (wrapped xDAI).",
-    "hatch_tribute_percentage": "Specifies what percentage of the funds raised by the Hatch go to the Non-redeemable Pool.",
-    "maximum_impact_hour_rate": "The theoretical maximum rate that Impact Hour token holders would be able to receive.",
-    "impact_hour_slope": "The parameter that effects the curvature of the Impact Hour graph, also the amount needed to be collected to reach 1/2 of the Max IH Rate divided by total IH.",
-    "support_required_percentage": "Percentage of tokens need to vote yes (of the tokens that were used to vote) for a proposal to pass.",
-    "minimum_accepted_quorum_percentage": "The percent of the total supply of the token that needs to vote yes in a vote in order for it to be able to be passed.",
-    "vote_duration_days": "This is the period of time it takes for a vote to pass or fail.",
-    "vote_buffer_hours": "The amount of time that must pass between the start of subsequent votes.",
-    "rage_quit_hours": "The period of time after a vote passes before the vote can be executed. This gives hatchers that don’t approve of the vote an opportunity to leave the hatch before vote execution.",
-    "tollgate_fee_xdai": "Requires a user to deposit this amount before creating a vote, this fee goes to the funding pool vault.",
+    "target_raise": {
+        "href": "https://forum.tecommons.org/t/hatch-goals-deep-dive/62",
+        "text": "The amount of wxDai YOU believe the TEC will collect used to aid design.",
+    },
+    "min_raise": {
+        "href": "https://forum.tecommons.org/t/hatch-goals-deep-dive/62",
+        "text": "The minimum amount of wxDai that needs to be collected during the hatch for it to succeed, if less than this is collected, 100% of the wxDai will be refunded.",
+    },
+    "max_raise": {
+        "href": "https://forum.tecommons.org/t/hatch-goals-deep-dive/62",
+        "text": "The maximum amount of wxDai that can be sent to the Hatch.",
+    },
+    "hatch_oracle_ratio": {
+        "href": "https://forum.tecommons.org/t/cstk-ratio-deep-dive/141",
+        "text": "The amount of wxDAI (wrapped xDAI) each CSTK Token holder is able to send to the Hatch per CSTK they have.",
+    },
+    "hatch_period_days": {
+        "href": "https://forum.tecommons.org/t/hatch-time-limit-deep-dive/63",
+        "text": "How long the Hatch is open to collect funds.",
+    },
+    "hatch_exchange_rate": {
+        "href": "https://forum.tecommons.org/t/hatch-exchange-rate/190",
+        "text": "The amount of TESTTECH that are obtained for 1 wxDAI (wrapped xDAI).",
+    },
+    "hatch_tribute_percentage": {
+        "href": "https://forum.tecommons.org/t/hatch-tribute-deep-dive/65",
+        "text": "Specifies what percentage of the funds raised by the Hatch go to the Non-redeemable Pool.",
+    },
+    "maximum_impact_hour_rate": {
+        "href": "https://forum.tecommons.org/t/impact-hour-rewards-deep-dive/90/10",
+        "text": "The theoretical maximum rate that Impact Hour token holders would be able to receive.",
+    },
+    "impact_hour_slope": {
+        "href": "https://forum.tecommons.org/t/impact-hour-rewards-deep-dive/90/10",
+        "text": "The parameter that effects the curvature of the Impact Hour graph, also the amount needed to be collected to reach 1/2 of the Max IH Rate divided by total IH.",
+    },
+    "support_required_percentage": {
+        "href": "https://forum.tecommons.org/t/support-required-deep-dive/108",
+        "text": "Percentage of tokens need to vote yes (of the tokens that were used to vote) for a proposal to pass.",
+    },
+    "minimum_accepted_quorum_percentage": {
+        "href": "https://forum.tecommons.org/t/minimum-quorum-required/133",
+        "text": "The percent of the total supply of the token that needs to vote yes in a vote in order for it to be able to be passed.",
+    },
+    "vote_duration_days": {
+        "href": "https://forum.tecommons.org/t/vote-duration-deep-dive/134",
+        "text": "This is the period of time it takes for a vote to pass or fail.",
+    },
+    "vote_buffer_hours": {
+        "href": "https://forum.tecommons.org/t/vote-proposal-buffer/132",
+        "text": "The amount of time that must pass between the start of subsequent votes.",
+    },
+    "rage_quit_hours": {
+        "href": "https://forum.tecommons.org/t/vote-execution-delay-and-what-it-means/130/2",
+        "text": "The period of time after a vote passes before the vote can be executed. This gives hatchers that don’t approve of the vote an opportunity to leave the hatch before vote execution.",
+    },
+    "tollgate_fee_xdai": {
+        "href": "https://forum.tecommons.org/t/tollgate-fee-deep-dive/128",
+        "text": "Requires a user to deposit this amount before creating a vote, this fee goes to the funding pool vault.",
+    }
 }
